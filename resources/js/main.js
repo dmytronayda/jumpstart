5008fbefa561b051880088523ca7472d6cefc9c9
Dmytro Nayda <dima.nayda.1993@gmail.com>
2018-02-11 12:09:59 +0200

    Add 'arrows: false' configure to remove Prev/Next Arrows in jumbotron slider.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
-    slidesToShow: 1, 
+    slidesToShow: 1,
     autoplay: true,
     autoplaySpeed: 4000,
-    accessibility: false,
+    arrows: false,
     dragable: false,
     pauseOnHover: false,
     pauseOnFocus: false
