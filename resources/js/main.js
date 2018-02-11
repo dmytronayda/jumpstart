788f4a74a063e8de600f94b2d8b114fe62518e7e
Dmytro Nayda <dima.nayda.1993@gmail.com>
2018-02-11 11:59:20 +0200

    Link 'slick' plugin for slider effect on jumbotron.

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    slidesToShow: 1, 
+    autoplay: true,
+    autoplaySpeed: 4000,
+    accessibility: false,
+    dragable: false,
+    pauseOnHover: false,
+    pauseOnFocus: false
+  });
+});
