49821489743f07111b362b3dc9af8f30edb38a9f
Dmytro Nayda <dima.nayda.1993@gmail.com>
2018-02-12 16:32:28 +0200

    Update jQuery script for navigation.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,32 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
-    slidesToShow: 1,
     autoplay: true,
     autoplaySpeed: 4000,
     arrows: false,
-    dragable: false,
-    pauseOnHover: false,
-    pauseOnFocus: false
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
   });
 });
-
-
-  // var prev = 0;
-  //
-  // $(window).scroll(function(event) {
-  //   var scrollTop = $this.scrollTop();
-  //
-  //   if (scrollTop - prev > 50) {
-  //     var navBarHeight = $('.navbar').css('height');
-  //     $('.navbar').animate({top: '-' + navBarHeight}, 150);
-  //     prev = scrollTop;
-  //   }
-  //
-  //   else (prev - scrollTop > 50) {
-  //     $('.navbar').animate({top: '-' - navBarHeight}, 150);
-  //     prev = scrollTop;
-  //   }
-  //
-  //
-  // });
